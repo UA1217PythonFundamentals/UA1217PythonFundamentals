b3c48719218b56fe89a49c17eebb4533f32ce7a2
Serhiid <serhiid.ua@gmail.com>
2024-07-01 00:26:50 +0300

    hw6 serhiidemchenko

---
+++ hw/hw06/hw06_serhiidemchenko.py
@@ -0,0 +1,24 @@
+
+#task 1
+
+t = [i for i in range(10) if not i % 2]
+print(t)
+
+t = [i for i in range(10) if not i % 3 if i % 2]
+print(t)
+
+t = [i for i in range(10) if not i % 3 if i % 2]
+print(t)
+
+# task 2
+
+login = input("Login: ")
+
+d = {"login":login}
+while True:
+    if d["login"] == "First":
+        print("Greed!")
+        break
+    else:
+        print("Error. Try another login")
+        break
