7754e452abfb9dce6d5db11e032ab469a90cf67e
Denys Ocheretnyy <denysoch@gmail.com>
2024-07-19 17:08:05 +0300

    add fix to hw4

--- hw/hw04/den1389/04.1_den1389.py
+++ hw/hw04/den1389/04.1_den1389.py
@@ -1,6 +1,6 @@
 temp = int(input("Enter the temperature in Celsius: " )) 
 if temp > -273.15:
     f = (temp * 9/5) + 32
-    print(f"{temp}°С is equivalent to {f}°F ")
+    print(f"{temp}°C is equivalent to {f}°F ")
 else:
     print("Error: The temperature is below absolute zero (-273.15°C)")
