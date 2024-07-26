3ed1660b16de34f0776668f68f7af920558011a1
Khrystyna Yaremchuk <kyaremchuk01@gmail.com>
2024-07-26 14:35:58 -0400

    hw/hw09/kyarem/9.01 Task.py

--- hw/hw09/kyarem/9.02 Task.py
+++ hw/hw09/kyarem/9.02 Task.py
@@ -34,13 +34,13 @@ while run:
 
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and COORD_X > 0:
         COORD_X = COORD_X-DELTA_STEP
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and COORD_X < WIDTH_DISPLAY - WIDTH_RECTANGLE:
         COORD_X = COORD_X+DELTA_STEP
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] and COORD_Y > 0:
         COORD_Y = COORD_Y-DELTA_STEP
-    if keys[pygame.K_DOWN]:
+    if keys[pygame.K_DOWN] and COORD_Y < HEIGHT_DISPLAY - HEIGHT_RECTANGLE:
         COORD_Y = COORD_Y+DELTA_STEP
 
 
