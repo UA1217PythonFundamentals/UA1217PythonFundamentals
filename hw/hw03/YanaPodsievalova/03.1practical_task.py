9f1186560017e05b0c9c8397a87600d7ac472d94
“Yana <podsievalovayana@gmail.com>
2024-08-22 09:52:36 +0300

    YanaPodsievalova

--- hw/hw03/YanaPodsievalova/03.1practical_task.py
+++ hw/hw03/YanaPodsievalova/03.1practical_task.py
@@ -1,3 +1,4 @@
+# 1
 python_philosophy = """Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
@@ -18,4 +19,39 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!"""
 
-print(type(python_philosophy))
+#print(type(python_philosophy)) #self check
+
+#number of "better", "never", "is"
+sub1 = "better"
+sub2 = "never"
+sub3 = "is"
+print(python_philosophy.count(sub1))
+print(python_philosophy.count(sub2))
+print(python_philosophy.count(sub3))
+
+#all text in upper case
+
+print(python_philosophy.upper())
+
+#replace "i" with "&"
+print(python_philosophy.replace("i","&"))
+
+# 2 find the product of the digits of this number
+number = 1234
+conv = str(number)
+digits = int(conv[0]) * int(conv[1]) * int(conv[2]) * int(conv[3])
+print(digits)
+#write the number in reverse order
+reversed_number = conv[::-1]
+print(reversed_number)
+#in ASC
+asc_number=''.join(sorted(reversed_number))
+print(asc_number)
+
+# 3 Interchange the values ​of two variables without using the third variable.
+a = 'cat'
+b = 'dog'
+print(a + b)
+
+a, b = a, b
+print(a + b)
